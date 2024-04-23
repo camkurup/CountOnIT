6801d2b48a1bc43601a07c5f725d22e218fd1393
Camilla Kurup <cku@seas-nve.dk>
2024-04-23 14:04:59 +0200

    Efter fejl i min githåndtering har jeg måtte gendarbejde det som gik tabt.- JKEg er nogenlunde med nu.

--- CountOnIT/ViewPages/UsagePage.xaml.cs
+++ CountOnIT/ViewPages/UsagePage.xaml.cs
@@ -2,33 +2,9 @@ namespace CountOnIT.Views;
 
 public partial class UsagePage : ContentPage
 {
-    bool isView1Visible = true;
 
     public UsagePage()
     {
         InitializeComponent();
-    }
-
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-        // Skift mellem View1 og View2
-        if (isView1Visible)
-            ShowView2();
-        else
-            ShowView1();
-
-        // Skift tilstanden for isView1Visible
-        isView1Visible = !isView1Visible;
-    }
-    private void ShowView1()
-    {
-        // Vis View1
-        ProductView.Content = new ProductListView();
-    }
-
-    private void ShowView2()
-    {
-        // Vis View2
-        ProductView.Content = new ProductView();
     }
 }
