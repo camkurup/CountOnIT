ffa2149ee72be30c72aada38f7a26be873b509c9
Camilla Kurup <cku@seas-nve.dk>
2024-05-01 12:36:36 +0200

    har slete datamock fordi jeg ønsker at få noget sat op som virker, men uden det kode som ikke virker

--- CountOnIT/MainPage.xaml.cs
+++ CountOnIT/MainPage.xaml.cs
@@ -1,30 +1,16 @@
-﻿using CountOnIT.Data;
-
+﻿
 namespace CountOnIT
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
-        public MainPage(DataMock write, DataMock read)
+       
+        public MainPage()
         {
             InitializeComponent();
-            Console.WriteLine("KÆMPEE TEST");
-            write.Write();
-            read.Read();
+            
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            //if (count == 1)
-                //CounterBtn.Text = $"Clicked {count} time";
-           // else
-                //CounterBtn.Text = $"Clicked {count} times";
-
-            //SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+       
     }
 
 }
